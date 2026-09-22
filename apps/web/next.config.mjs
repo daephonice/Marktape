1e14e8089e62015141b00ad5cb67d504abcd6380
Ice ❄️🪸 <tinuolagad@gmail.com>
2026-09-22 13:15:46 +0100

    Delete apps directory

--- apps/web/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@marktape/core"],
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-};
-
-export default nextConfig;
